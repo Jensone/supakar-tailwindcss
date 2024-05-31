96343d2cc6af09cf7448bc5fbc41a369cc0decd2
Jensone <hello@jensone.com>
2024-05-31 17:00:54 +0200

    Update of tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './output.css' // On importe le fichier Tailwind CSS final
-import './index.css'
+import './output.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
